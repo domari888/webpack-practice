b3e8f1c48930090fd0b35527b103dafa1d709865
damari <motokin128@gmail.com>
2021-07-09 16:45:12 +0900

    babel の動作確認

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-// 変数 message を宣言し， 'Hello, World' を代入
-const message = 'Hello, World!';
-// ブラウザに 'Hello, World!' のダイアログを表示
-alert(message);
-// コンソールに 'Hello, World!' を出力
-console.log(message);
+// // 変数 message を宣言し， 'Hello, World' を代入
+// const message = 'Hello, World!';
+// // ブラウザに 'Hello, World!' のダイアログを表示
+// alert(message);
+// // コンソールに 'Hello, World!' を出力
+// console.log(message);
 
-import $ from 'jquery';
-alert($.fn.jquery);
+// import $ from 'jquery';
+// alert($.fn.jquery);
+
+let num = 0;
+num ||= 1;
+
+let num2 = 0;
+num2??= 1;
+
+console.log(`num は ${num}`);
+console.log(`num2 は ${num2}`);
