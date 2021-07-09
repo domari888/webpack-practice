d6bec2bab754f7ebaa9c931897a9701412ebec34
damari <motokin128@gmail.com>
2021-07-09 16:21:53 +0900

    babel のインストールと初期設定

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,16 @@ module.export = {
     // 出力先のディレクトリ名
     path: outputPath,
   },
+  module: {
+    rules: [
+      {
+        // 対象ファイル(.js, .mjs で終わるファイル全て)
+        test: /\.m?js$/i,
+        // node_modules ディレクトリは対象外にする
+        exclude: /node_modules/,
+        // 使用するローダー
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
