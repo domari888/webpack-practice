f2af63ce8a8c2d8d5ce83ffb96bf499e9fa1d166
damari <motokin128@gmail.com>
2021-07-09 15:25:41 +0900

    build を実行

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// 変数 message を宣言し， 'Hello, World' を代入
+const message = 'Hello, World!';
+// ブラウザに 'Hello, World!' のダイアログを表示
+alert(message);
+// コンソールに 'Hello, World!' を出力
+console.log(message);
+
+import $ from 'jquery';
+alert($.fn.jquery);
